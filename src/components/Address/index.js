b9edcaab3c64e47856abb97852c6eedfecd747ed
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-26 11:08:35 -0300

    feat: Address component

---
+++ src/components/Address/index.js
@@ -0,0 +1,16 @@
+const Address = ({ address }) => {
+    return (
+        <div className="w-5/6 lg:w-3/6 p-5 border bg-gray-200 border-gray-300 rounded-md drop-shadow flex flex-col justify-center items-center dark:bg-neutral-800 dark:border-neutral-700">
+            <ul>
+                <li className="li">CEP: {address.cep}</li>
+                <li className="li">State: {address.state}</li>
+                <li className="li">City: {address.city}</li>
+                <li className="li">Neighborhood: {address.neighborhood}</li>
+                <li className="li">Street: {address.street}</li>
+                <li className="li">DDD: {address.ddd}</li>
+            </ul>
+        </div>
+    );
+};
+
+export default Address;
