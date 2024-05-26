af7b20343abf9b6a4bc7811e7c8665613d4f920b
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-26 11:10:02 -0300

    feat: Main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
     <div 
-      className="h-dvh bg-cover bg-center"
+      className="h-dvh bg-cover bg-center flex flex-col items-center gap-14"
       style={{ backgroundImage: "url(/assets/cep-finder-background.jpg)" }}
     >
       <Header />
+      <Main />
     </div>
   );
 };
