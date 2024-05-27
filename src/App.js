595187ddea17449552740c927e299b6113d0cfaf
Matheus-Pombeiro <pombeiromatheus@gmail.com>
2024-05-27 20:38:36 -0300

    fix: the App's height

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Main from "./components/Main";
 const App = () => {
   return (
     <div 
-      className="h-dvh bg-cover bg-center flex flex-col items-center gap-14"
+      className="h-screen bg-cover bg-center flex flex-col items-center gap-14"
       style={{ backgroundImage: "url(/assets/cep-finder-background.jpg)" }}
     >
       <Header />
